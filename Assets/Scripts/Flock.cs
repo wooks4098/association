cdb89df65fcf752199747e803d8aa4d67b608299
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2021-06-03 01:51:06 +0900

    Flocking

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public float speed;
+    float rotationSpeed = 4.0f;
+    Vector3 averageHeading;
+    Vector3 averagePosition;
+    float neighbourDistance = 5.0f;
+
+    bool turning = false;
+
+    void Start()
+    {
+        speed = Random.Range(1f,2);
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Vector3.zero) >= GlobalFlock.tankSize)
+            turning = true;
+        else
+            turning = false;
+        if(turning)
+        {
+            Vector3 direction = Vector3.zero - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+
+            speed = Random.RandomRange(1, 2);
+        }
+        else
+        {
+            if(Random.Range(0,5)<1)
+                ApplyRules();
+        }
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+
+    void ApplyRules()
+    {
+        GameObject[] gos;
+        gos = GlobalFlock.AllPrefab;
+
+        Vector3 vcentre = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.1f;
+
+        Vector3 goalPos = GlobalFlock.goalPos;
+
+        float dist;
+
+        int groupSize = 0;
+        foreach(GameObject go in gos)
+        {
+            if(go != this.gameObject)
+            {
+                dist = Vector3.Distance(go.transform.position, this.transform.position);
+                if(dist <= neighbourDistance)
+                {
+                    vcentre += go.transform.position;
+                    groupSize++;
+                    if(dist <1.0f)
+                    {
+                        vavoid = vavoid + (this.transform.position - go.transform.position);
+                    }
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+        }
+
+        if (groupSize>0)
+        {
+            vcentre = vcentre / groupSize + (goalPos - this.transform.position);
+            speed = gSpeed / groupSize;
+
+            Vector3 direction = (vcentre + vavoid) - transform.position;
+            if (direction != Vector3.zero)
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/GlobalFlock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalFlock : MonoBehaviour
+{
+    public GameObject Prfab;
+    public GameObject goalPrefab;
+    static int num = 30;
+    public static GameObject[] AllPrefab = new GameObject[num];
+
+    public static int tankSize = 10;
+
+    public static Vector3 goalPos = Vector3.zero;
+
+    private void Start()
+    {
+        for(int i = 0; i<num; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-tankSize, tankSize), 
+                                      Random.Range(-tankSize, tankSize), 
+                                      Random.Range(-tankSize, tankSize));
+            AllPrefab[i] = Instantiate(Prfab, pos, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        if(Random.Range(0,10000)<50)
+        {
+            goalPos = new Vector3(Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize),
+                                      Random.Range(-tankSize, tankSize));
+            goalPrefab.transform.position = goalPos;
+        }
+    }
+
+}
